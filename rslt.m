bb0b59524ae0cac377610d844b3462daa5862eaa
Jeffrey Benjamin Brown <jeffbrown.the@gmail.com>
2018-11-08 21:46:19 -0500

    code the types

--- rslt.m
+++ rslt.m
@@ -2,12 +2,19 @@
 :- interface.
 :- import_module io, int, string, list.
 :- pred main(io::di, io::uo) is det.
-:- type index == int.
+:- type index == int. % each expr has one of these, like an address
 :- type arity == int.
-:- type position == int.
-:- type expr ---> word( string )
-                ; tplt( list( string ) )
-                ; rel.
+:- type expr ---> word( string ) ; tplt ; rel.
+:- type role ---> roleTemplate ; roleMember( int ).
+:- type position ---> (index, role). % Like a position at an employer --
+                                     % includes host and role played there.
+                                     % Index should be of a Rel.
+:- type exprImg ---> % An image (diagram, mugshot) of an expr --
+                     % something that lets you find it.
+    imgIndex
+    ; imgWord( string )
+    ; imgTemplate( list( exprImg ) )
+    ; imgRel( exprImg, list( exprImg ) ).
 :- implementation.
 :- import_module hash_table.
 
