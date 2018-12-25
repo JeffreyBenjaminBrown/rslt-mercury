2d391e9cd15933ed88edad71d29969972e03afc2
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-12-25 01:21:04 -0500

    run PQuery : compiled, untested   :- pred runPQuery( list(int), pQuery, int ).   :- mode runPQuery( in,        in,     out ) is nondet.

--- toy/query.m
+++ toy/query.m
@@ -1,16 +1,32 @@
 :- module query.
 :- interface.
+:- import_module bool.
 :- import_module int.
 :- import_module list.
 
 :- type pQuery ---> qElt( int )
       ; qFind( func( list(int) ) = list(int) ).
 
+:- type nQuery ---> qNot( int )
+      ; qCond( func( list(int), int ) = bool ).
+
 :- pred runPQuery( list(int), pQuery, int ).
-:- mode runPQuery( in, in, out ) is nondet.
+:- mode runPQuery( in,        in,     out ) is nondet.
+
+:- pred testNQuery( list(int), nQuery, int, bool ).
+:- mode testNQuery( in,        in,     in,  out ) is nondet.
 
 
 :- implementation.
+
+testNQuery( Space, qCond(Q) , Elt, Res ) :-
+  Q( Space, Elt ) = Res.
+testNQuery( Space, qNot(Int), Elt, yes ) :-
+  list.member( Elt, Space )
+  , Elt \= Int.
+testNQuery( Space, qNot(Int), Elt, no  ) :-
+  not( list.member( Elt, Space ) )
+  ; Elt = Int.
 
 runPQuery( List, qElt( Elt ), Elt ) :-
   list.member( Elt, List ).
