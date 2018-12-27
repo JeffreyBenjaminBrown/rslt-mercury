1a3bdcb89a529e4f209d2e611079054f33a13fc0
JeffreyBenjaminBrown <jeffbrown.the@gmail.com>
2018-12-26 22:58:44 -0500

    :- pred findable( query, bool ). :- mode findable( in, out ) is det.

--- toy/query.m
+++ toy/query.m
@@ -9,11 +9,14 @@
 
 :- type qCond ---> qCond( func( int ) = bool).
 
-:- type query ---> qFind( qFind ). % TODO next
-%                ; qCond( qCond ).
-%                ; qAnd( list(query) )
+:- type query ---> qFind( qFind ) % TODO next
+                 ; qCond( qCond )
+                 ; qAnd( list(query) ).
 %                ; qOr( list(query) )
 %                ; qVar( string ).
+
+:- pred runQuery( list(int), query, list(int) ).
+:- mode runQuery( in,        in,    out       ) is semidet.
 
 :- pred runQFind( list(int), qFind, list(int) ).
 :- mode runQFind( in,        in,    out       ) is det.
@@ -21,11 +24,19 @@
 :- pred checkQCond( qCond, int, bool ).
 :- mode checkQCond( in,     in,  out ) is det.
 
-%:- pred runQuery( list(int), query, list(int) ).
-%:- mode runQuery( in,        in,    out       ) is det.
-
 
 :- implementation.
+
+:- pred findable( query, bool ).
+:- mode findable( in, out ) is det.
+findable( qFind(_)  , yes ).
+findable( qCond(_)  , no ).
+findable( qAnd( Qs ), Res ) :-
+  list.map( findable, Qs, Findables )
+  , Res = bool.and_list( Findables ).
+
+runQuery( Space, qFind( QF ), Res ) :-
+  runQFind( Space, QF, Res ).
 
 runQFind( Space, qElt( Elt ),  Res          ) :-
   Res = ( if list.member( Elt, Space )
